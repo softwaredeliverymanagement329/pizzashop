f92df8424a52d68cd5d6e6d56411dd100ecd9c1e
Lee <lee.jensen@agilix.com>
2024-02-09 10:39:18 -0700

    enhance(ci) run tests

--- tests/orderPizzas.spec.js
+++ tests/orderPizzas.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('testWelcomeButton', async ({ page }) => {
   // Navigate to the home page
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
 
   // Get started on the homepage
   await expect(page).toHaveTitle('Pizza Shop');
