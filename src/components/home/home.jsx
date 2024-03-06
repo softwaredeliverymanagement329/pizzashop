5d486ddb90bc2eb69a540551ee43c56b2f855633
Lee <lee.jensen@agilix.com>
2024-03-06 13:46:33 -0700

    fix(ci) deployment script needs to be on job

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -10,7 +10,7 @@ export function Home({ userState }) {
         Welcome to the ultimate online pizza shop. Use our award winning <i>Pizza Shop™</i> application to order your
         pizzas on your phone or laptop and they magically appear.
       </p>
-      <h2>Love it guarantee</h2>
+      <h2>Love it guarantee!</h2>
       <p>
         At the Pizza Shop we always make it right! If we do not deliver a fresh, perfectly cooked, pizza to your device
         in under 500 milliseconds we will bake you another pizza <b>and</b> refund your money.
